8b1cf7b90f6cd43ae6d0b1211cc0b6c4d0212b09
Michel B <michelb@JB-MBA.local>
2018-02-05 14:02:16 -0500

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Black Bean');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Single Patty', true);
